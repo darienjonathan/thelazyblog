b227220e03aabe286f065bc8e5ae35c5cf8828b4
darienjonathan <darien-winata@kayac.com>
2017-09-21 00:03:57 +0900

    activeadmin

--- app/models/content_image.rb
+++ app/models/content_image.rb
@@ -1,4 +1,11 @@
 class ContentImage < ApplicationRecord
   belongs_to :blog
   mount_uploader :url, ContentImageUploader
+
+  IMG_CLASS_ARRAY = ["normal-img", "small-img"]
+
+  def class_valid?
+    errors.add(:field, 'The allowed class is only "normal-img" or "small-img"') unless IMG_CLASS_ARRAY.include? class_attr
+  end
+
 end
