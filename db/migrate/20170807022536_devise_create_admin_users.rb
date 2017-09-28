4b4f4444cd4d4f1a8db539d3517e916522ba8b09
darienjonathan <darien-winata@kayac.com>
2017-09-28 12:22:20 +0900

    devise index length

--- db/migrate/20170807022536_devise_create_admin_users.rb
+++ db/migrate/20170807022536_devise_create_admin_users.rb
@@ -34,8 +34,8 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration[5.1]
       t.timestamps null: false
     end
 
-    add_index :admin_users, :email,                unique: true
-    add_index :admin_users, :reset_password_token, unique: true
+    add_index :admin_users, :email,                unique: true, length: 767
+    add_index :admin_users, :reset_password_token, unique: true, length: 767
     # add_index :admin_users, :confirmation_token,   unique: true
     # add_index :admin_users, :unlock_token,         unique: true
   end
