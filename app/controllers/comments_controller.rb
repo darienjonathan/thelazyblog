9f6262eaf71620784c371e6dabd2c43b2bdf3ece
darienjonathan <darien-winata@kayac.com>
2018-01-15 11:49:37 +0900

    associate comments to blogs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,11 @@
 class CommentsController < InheritedResources::Base
 
+  def create
+    blog = Blog.find(params[:blog_id])
+    redirect_to blog_path blog if blog.comments.create!(comment_params)
+    
+  end
+
   private
 
     def comment_params
