8b2082a4b044333481a95a4eb90f3d9d54b47b6e
darienjonathan <darien-winata@kayac.com>
2018-05-12 16:56:58 +0900

    move mobile css from js to css

--- app/assets/javascripts/blogs/index.js
+++ app/assets/javascripts/blogs/index.js
@@ -3,13 +3,6 @@ const comment = require('./lib/comment');
 
 $(() => {
   $(document).foundation();
-
-  if (window.matchMedia('(max-width: 600px)').matches) {
-    $('.blogs--header').css("height", window.innerHeight);
-    $('.blogs--title').css("padding-top", window.innerHeight*0.335);
-    $('.columns').css("height", window.innerHeight*0.6);
-  }
-
   language();
   comment();
 });
