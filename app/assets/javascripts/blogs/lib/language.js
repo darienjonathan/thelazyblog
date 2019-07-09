0c72791b825d99b4f45e5c94b26fef7d018258b1
darienjonathan <darien-winata@kayac.com>
2019-07-09 20:41:54 +0900

    fix lang

--- app/assets/javascripts/blogs/lib/language.js
+++ app/assets/javascripts/blogs/lib/language.js
@@ -1,7 +1,20 @@
 module.exports = () => {
-  let en = document.querySelectorAll('.en'),
-  id = document.querySelectorAll('.id'),
+  let id = document.querySelectorAll('.id'),
+  en = document.querySelectorAll('.en'),
   jp = document.querySelectorAll('.jp');
+
+  if(id.length > 0){
+    document.getElementById('lang_id').addEventListener('click', () => {
+      $(this).addClass('active');
+      $('#lang_en').removeClass('active');
+      $('#lang_jp').removeClass('active');
+      for(let i = 0; i < id.length; i++){
+        id[i].style.display = "block";
+        if(en.length > 0){en[i].style.display = "none";}
+        if(jp.length > 0){jp[i].style.display = "none";}
+      }
+    })
+  }
   
   if(en.length > 0){
     document.getElementById('lang_en').addEventListener('click', () => {
@@ -16,25 +29,12 @@ module.exports = () => {
     })
   }
 
-  if(id.length > 0){
-    document.getElementById('lang_id').addEventListener('click', () => {
-      $(this).addClass('active');
-      $('#lang_en').removeClass('active');
-      $('#lang_jp').removeClass('active');
-      for(let i = 0; i < en.length; i++){
-        id[i].style.display = "block";
-        if(en.length > 0){en[i].style.display = "none";}
-        if(jp.length > 0){jp[i].style.display = "none";}
-      }
-    })
-  }
-
   if(jp.length > 0){
     document.getElementById('lang_jp').addEventListener('click', () => {
       $(this).addClass('active');
       $('#lang_en').removeClass('active');
       $('#lang_id').removeClass('active');
-      for(let i = 0; i < en.length; i++){
+      for(let i = 0; i < jp.length; i++){
         jp[i].style.display = "block";
         if(en.length > 0){en[i].style.display = "none";}
         if(id.length > 0){id[i].style.display = "none";}
