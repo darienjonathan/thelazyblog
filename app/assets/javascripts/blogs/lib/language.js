28e38b16929f91898f9aefd092d879e1054781b1
darienjonathan <darien-winata@kayac.com>
2019-07-11 14:28:22 +0900

    language logic

--- app/assets/javascripts/blogs/lib/language.js
+++ app/assets/javascripts/blogs/lib/language.js
@@ -1,44 +1,49 @@
 module.exports = () => {
-  let id = document.querySelectorAll('.id'),
-  en = document.querySelectorAll('.en'),
-  jp = document.querySelectorAll('.jp');
+  const langList = ['id', 'en', 'jp']
+  let textElementsObj = {}
+  let langSelectorElementsObj = {}
 
-  if(id.length > 0){
-    document.getElementById('lang_id').addEventListener('click', () => {
-      $('#lang_id').addClass('active');
-      $('#lang_en').removeClass('active');
-      $('#lang_jp').removeClass('active');
-      for(let i = 0; i < id.length; i++){
-        id[i].style.display = "block";
-        if(en.length > 0){en[i].style.display = "none";}
-        if(jp.length > 0){jp[i].style.display = "none";}
-      }
-    })
-  }
-  
-  if(en.length > 0){
-    document.getElementById('lang_en').addEventListener('click', () => {
-      $('#lang_en').addClass('active');
-      $('#lang_id').removeClass('active');
-      $('#lang_jp').removeClass('active');
-      for(let i = 0; i < en.length; i++){
-        en[i].style.display = "block";
-        if(id.length > 0){id[i].style.display = "none";}
-        if(jp.length > 0){jp[i].style.display = "none";}
-      }
+  langList.forEach(lang => {
+    textElementsObj[lang] = document.querySelectorAll(`.${lang}`)
+    langSelectorElementsObj[lang] = $(`#lang_${lang}`)
+  })
+
+  const toggleLang = (selectedLang) => {
+    langList.forEach(lang => {
+      const display = lang === selectedLang ? 'block' : 'none'
+      setDisplay(textElementsObj[lang], display)
     })
   }
 
-  if(jp.length > 0){
-    document.getElementById('lang_jp').addEventListener('click', () => {
-      $('#lang_jp').addClass('active');
-      $('#lang_en').removeClass('active');
-      $('#lang_id').removeClass('active');
-      for(let i = 0; i < jp.length; i++){
-        jp[i].style.display = "block";
-        if(en.length > 0){en[i].style.display = "none";}
-        if(id.length > 0){id[i].style.display = "none";}
+  const setDisplay = (elArr, display) => {
+    for(let i = 0; i < elArr.length; i++) {
+      elArr[i].style.display = display
+    }
+  }
+
+  const toggleLangSelectorEffect = (selectedLang) => {
+    langList.forEach(lang => {
+      const action = lang === selectedLang ? 'addClass' : 'removeClass'
+      langSelectorElementsObj[lang][action]('active')
+    })
+  }
+
+  const initialize = () => {
+    let shown = false
+    langList.forEach(lang => {
+      if(textElementsObj[lang].length > 0) {
+        if(!shown) {
+          toggleLang(lang)
+          shown = true
+        }
+        langSelectorElementsObj[lang].click(() => {
+          toggleLangSelectorEffect(lang)
+          toggleLang(lang)
+        })
       }
     })
-  }    
+
+  }
+
+  initialize();
 };
